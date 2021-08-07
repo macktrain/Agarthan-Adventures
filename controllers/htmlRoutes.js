feb097b648a4afa319bfd2aa8db829e661322647
Drock4440 <daltonrothrock8@gmail.com>
2021-08-07 13:34:55 -0600

    styling and fixing homepage and build page

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,25 +1,24 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
-
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
+router.get('/', (req,res) => {
+  res.render('homepage');
+})
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
+//     const users = userData.map((project) => project.get({ plain: true }));
+//     res.render('homepage', {
+//       users,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
@@ -27,18 +26,13 @@ router.get('/login', (req, res) => {
   }
   res.render('login');
 });
-
 router.get('/characterBuild', (req,res) => {
   res.render ('characterBuild');
 })
-
 router.get('/characterMenu', (req,res) => {
   res.render ('characterMenu');
 })
-
 router.get('/characterView', (req,res) => {
   res.render ('characterView');
 })
-
-
 module.exports = router;
