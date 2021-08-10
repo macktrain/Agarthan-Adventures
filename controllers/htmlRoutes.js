8b937b059c9bf7eb301c0a87f1a122ee3c5c57b8
Drock4440 <daltonrothrock8@gmail.com>
2021-08-09 19:20:35 -0600

    homepage final

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,27 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
-<<<<<<< HEAD
-router.get('/', (req,res) => {
-  res.render('homepage');
-})
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
-//     const users = userData.map((project) => project.get({ plain: true }));
-//     res.render('homepage', {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-router.get('/login', (req, res) => {
-=======
 
 
 router.get('/', (req,res) => {
@@ -35,25 +14,12 @@ router.get('/', (req,res) => {
 });
 
 router.get('/login', async (req, res) => {
->>>>>>> a82dd3ec8c1ad995dd5770b6df11e71758efd564
   if (req.session.logged_in) {
     res.redirect('/gameMenu');
     return;
   }
   res.render('login');
 });
-<<<<<<< HEAD
-router.get('/characterBuild', (req,res) => {
-  res.render ('characterBuild');
-})
-router.get('/characterMenu', (req,res) => {
-  res.render ('characterMenu');
-})
-router.get('/characterView', (req,res) => {
-  res.render ('characterView');
-})
-module.exports = router;
-=======
 
 router.get('/signup', (req, res) => {
   if (req.session.logged_in) {
@@ -81,4 +47,3 @@ router.get('/characterView', (req,res) => {
 
 
 module.exports = router;
->>>>>>> a82dd3ec8c1ad995dd5770b6df11e71758efd564
